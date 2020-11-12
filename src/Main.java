d8a16fcaeae917db8f107987da90ac24eaebed3f
Aleksandr Osipov <eltisprim@gmail.com>
2020-11-12 20:28:47 +0300

    Upgrade cod

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        int BodyMassIndex = service.calculate(186, 10700);
+        int Height = 186;
+        int Weight = 10700;
+        int BodyMassIndex = service.calculate(Height, Weight);
         System.out.println("Ваш индекс массы тела: " + BodyMassIndex);
 
     }
